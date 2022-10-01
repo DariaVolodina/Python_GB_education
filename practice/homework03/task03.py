c4339b64e5dda6ab38b4031a4ebf9ce16c97864d
Daria <fedu@inbox.ru>
2022-10-02 00:41:50 +0300

    some changes made

--- practice/homework03/task03.py
+++ practice/homework03/task03.py
@@ -12,38 +12,50 @@
 
 
 def FractionalPart (list):
-    new_list = []
     fract_list = []
-    int_list = []
-    new_list2 = []
-
     num = 0
-    count = 0
-   
     for i in range(len(list)):
         if not list[i] == int(list[i]):
-           fract_list.append(list[i])
+            num = list[i] - int(list[i])
+            while num != int(num):
+                list[i] *= 10
+            fract_list.append(num)
+    return fract_list
 
-    print(fract_list)
 
-    for j in range(len(fract_list)):
-        num = int(fract_list[j])
-        int_list.append(num)
 
-    print(int_list)
+#     new_list = []
+#     fract_list = []
+#     int_list = []
+#     new_list2 = []
 
-    for e in range (len(fract_list)):
-        while fract_list[e] != int(fract_list[e]):
-            fract_list[e] *= 10
-            count += 1
-        new_list.append(fract_list[e])  
-        print(count)
-        new_list2.append(int_list[e] * 10 * count)
+#     num = 0
+#     count = 0
+   
+#     for i in range(len(list)):
+#         if not list[i] == int(list[i]):
+#            fract_list.append(list[i])
 
-    print(new_list)
-    print(new_list2)
+#     print(fract_list)
 
-    # return new_list
+#     for j in range(len(fract_list)):
+#         num = int(fract_list[j])
+#         int_list.append(num)
+
+#     print(int_list)
+
+#     for e in range (len(fract_list)):
+#         while fract_list[e] != int(fract_list[e]):
+#             fract_list[e] *= 10
+#             count += 1
+#         new_list.append(fract_list[e])  
+#         print(count)
+#         new_list2.append(int_list[e] * 10 * count)
+
+#     print(new_list)
+#     print(new_list2)
+
+#     # return new_list
 
 list1 = [1.1, 1.2, 3.1, 5, 10.01]
 print(list1)
