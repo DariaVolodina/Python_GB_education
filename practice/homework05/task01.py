849a36c948a68fd53c0d4ae032abb6e62faa9e44
Daria <fedu@inbox.ru>
2022-10-26 22:35:55 +0300

    task 01 done

--- practice/homework05/task01.py
+++ practice/homework05/task01.py
@@ -1,3 +1,35 @@
 # Напишите программу, удаляющую из текста
-# все слова, содержащие ""абв"".
+# все слова, содержащие "абв".
+# in
+# Number of words: 10
 
+# out
+# авб абв бав абв вба бав вба абв абв абв
+# авб бав вба бав вба
+
+from random import sample
+
+
+def createList (n: int):
+    list1 = [] # create word from list of symbols
+    list_of_words = [] # full list
+
+    for i in range(n):
+        for j in range(3):
+            list1 = ''.join(sample(['а', 'б', 'в'], k = 3))
+        list_of_words.append(list1)
+
+    return list_of_words 
+    
+
+def removeSequence (myList: list, s: str):
+    for i in myList:
+        if s in i:
+            myList.remove(i)
+    return myList
+
+first_list = createList(int(input('Input number of words: ')))
+print(*first_list)
+
+print('\nList with no "абв" sequence:')
+print(*removeSequence(first_list, 'абв'))
